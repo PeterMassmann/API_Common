dfb4f4a9ed9c477a223e92deb5d8f7d205a9f9fc
PeterMassmann <pedro@massmann.cl>
2024-07-30 20:31:12 -0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+val springCloudVersion by extra("2023.0.3")
+
+group = "com.easyspec"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    // spring boot
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/easyspec/templates/EndpointConfig.java
@@ -0,0 +1,72 @@
+package com.easyspec.templates;
+
+import java.util.List;
+import java.util.Map;
+
+public class EndpointConfig {
+    private final String path;
+    private final String type;
+    private final List<String> methods;
+    private final Map<String, String> pathVariablePatterns;
+    private final List<String> dependencies;
+
+    private EndpointConfig(String path, String type, List<String> methods, Map<String, String> pathVariablePatterns, List<String> dependencies) {
+        this.path = path;
+        this.type = type;
+        this.methods = methods;
+        this.pathVariablePatterns = pathVariablePatterns;
+        this.dependencies = dependencies;
+    }
+
+    private EndpointConfig(String path, String type, List<String> methods, Map<String, String> pathVariablePatterns) {
+        this.path = path;
+        this.type = type;
+        this.methods = methods;
+        this.pathVariablePatterns = pathVariablePatterns;
+        this.dependencies = List.of();
+    }
+
+    private EndpointConfig(String path, String type, List<String> methods) {
+        this.path = path;
+        this.type = type;
+        this.methods = methods;
+        this.pathVariablePatterns = Map.of();
+        this.dependencies = List.of();
+    }
+
+    private EndpointConfig(String path, String type, List<String> methods, List<String> dependencies) {
+        this.path = path;
+        this.type = type;
+        this.methods = methods;
+        this.pathVariablePatterns = Map.of();
+        this.dependencies = dependencies;
+    }
+
+    private EndpointConfig(String path, String type, String... methods) {
+        this.path = path;
+        this.type = type;
+        this.methods = List.of(methods);
+        this.pathVariablePatterns = Map.of();
+        this.dependencies = List.of();
+    }
+
+    public String getPath() {
+        return this.path;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public List<String> getMethods() {
+        return methods;
+    }
+
+    public Map<String, String> getPathVariablePatterns() {
+        return pathVariablePatterns;
+    }
+
+    public List<String> getDependencies() {
+        return dependencies;
+    }
+}
