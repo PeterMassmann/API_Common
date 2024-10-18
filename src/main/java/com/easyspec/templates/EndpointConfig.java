2e06bcc2a6264a84a64b5bce715d0c6faa064bf2
PeterMassmann <pedro@massmann.cl>
2024-10-18 14:57:22 -0300

    added requestedPermissions to EndpointConfig

--- src/main/java/com/easyspec/templates/EndpointConfig.java
+++ src/main/java/com/easyspec/templates/EndpointConfig.java
@@ -7,26 +7,33 @@ import java.util.Map;
 
 @Getter
 public class EndpointConfig {
+
+    /**
+     * The path of the endpoint.
+     */
     private final String path;
     private final String type;
     private final List<String> methods;
     private final Map<String, String> pathVariablePatterns;
     private final List<String> dependencies;
+    private final List<String> requestedPermissions; // The permissions that should be passed along to the microservice
 
-    public EndpointConfig(String path, String type, List<String> methods, Map<String, String> pathVariablePatterns, List<String> dependencies) {
+    public EndpointConfig(String path, String type, List<String> methods, Map<String, String> pathVariablePatterns, List<String> dependencies, List<String> requestedPermissions) {
         this.path = path;
         this.type = type;
         this.methods = methods;
         this.pathVariablePatterns = pathVariablePatterns;
         this.dependencies = dependencies;
+        this.requestedPermissions = requestedPermissions;
     }
 
-    public EndpointConfig(String path, String type, List<String> methods, Map<String, String> pathVariablePatterns) {
+    public EndpointConfig(String path, String type, List<String> methods, Map<String, String> pathVariablePatterns, List<String> requestedPermissions) {
         this.path = path;
         this.type = type;
         this.methods = methods;
         this.pathVariablePatterns = pathVariablePatterns;
         this.dependencies = List.of();
+        this.requestedPermissions = requestedPermissions;
     }
 
     public EndpointConfig(String path, String type, List<String> methods) {
@@ -35,6 +42,7 @@ public class EndpointConfig {
         this.methods = methods;
         this.pathVariablePatterns = Map.of();
         this.dependencies = List.of();
+        this.requestedPermissions = List.of();
     }
 
     public EndpointConfig(String path, String type, List<String> methods, List<String> dependencies) {
@@ -43,13 +51,6 @@ public class EndpointConfig {
         this.methods = methods;
         this.pathVariablePatterns = Map.of();
         this.dependencies = dependencies;
-    }
-
-    public EndpointConfig(String path, String type, String... methods) {
-        this.path = path;
-        this.type = type;
-        this.methods = List.of(methods);
-        this.pathVariablePatterns = Map.of();
-        this.dependencies = List.of();
+        this.requestedPermissions = List.of();
     }
 }
